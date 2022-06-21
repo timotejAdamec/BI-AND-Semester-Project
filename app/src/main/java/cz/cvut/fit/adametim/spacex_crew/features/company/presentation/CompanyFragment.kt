cd8c508aad64401421d72fa0087be57229959a83
Timotej Adamec <adametim@fit.cvut.cz>
2022-06-21 22:04:52 +0200

    master: phone call done

--- app/src/main/java/cz/cvut/fit/adametim/spacex_crew/features/company/presentation/CompanyFragment.kt
+++ app/src/main/java/cz/cvut/fit/adametim/spacex_crew/features/company/presentation/CompanyFragment.kt
@@ -1,21 +1,25 @@
 package cz.cvut.fit.adametim.spacex_crew.features.company.presentation
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
 import cz.cvut.fit.adametim.spacex_crew.R
 import cz.cvut.fit.adametim.spacex_crew.databinding.FragmentCompanyBinding
-import cz.cvut.fit.adametim.spacex_crew.databinding.FragmentCrewBinding
-import cz.cvut.fit.adametim.spacex_crew.features.crew.presentation.CrewAdapter
-import cz.cvut.fit.adametim.spacex_crew.features.crew.presentation.CrewState
-import cz.cvut.fit.adametim.spacex_crew.features.crew.presentation.CrewViewModel
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -70,13 +74,69 @@ class CompanyFragment : Fragment() {
                         Snackbar.make(
                             binding.root,
                             getString(R.string.company_error_loading_msg),
-                            Snackbar.LENGTH_SHORT)
+                            Snackbar.LENGTH_SHORT
+                        )
                             .show()
-                        Log.e(this.javaClass.simpleName,
-                            "Error loading resources = " + state.throwable.message, state.throwable)
+                        Log.e(
+                            this.javaClass.simpleName,
+                            "Error loading resources = " + state.throwable.message, state.throwable
+                        )
                     }
                 }
             }
+        }
+
+        applyButton.setOnClickListener {
+            applyButtonOnClick()
+        }
+    }
+
+    private fun applyButtonOnClick() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+            AlertDialog.Builder(requireContext())
+                .setTitle("Call recruiter permission")
+                .setMessage("In order to connect you with the recruiter we need to make a phone call")
+                .setPositiveButton("OK") { _, _ ->
+                    requestPermission()
+                }
+                .setNegativeButton("NO", null)
+                .show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    private val callPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions[Manifest.permission.CALL_PHONE] == true) {
+                dialSpaceXPhoneNumber()
+            } else {
+                permissionDenied()
+            }
+        }
+
+    private fun requestPermission() {
+        callPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.CALL_PHONE,
+            )
+        )
+    }
+
+    private fun permissionDenied() {
+        Snackbar.make(
+            requireView(),
+            getString(R.string.call_permission_denied_msg),
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    private fun dialSpaceXPhoneNumber() {
+        val intent = Intent(Intent.ACTION_CALL).apply {
+            data = Uri.parse("tel:123456789")
+        }
+        if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+            startActivity(intent)
         }
     }
 
